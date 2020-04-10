586237d2f7a7f0d1d5e698dc6b10ef0e411c4d07
KevenPotter <keven_potter@163.com>
2020-04-10 17:03:35 +0800

    Commit

---
+++ src/main/java/com/example/juc/SaleTicketDemo01.java
@@ -0,0 +1,24 @@
+package com.example.juc;
+
+class Ticket { // 资源类 = 实例变量 + 实例方法
+
+    private int number = 30;
+    // List list = new ArrayList();
+
+    public void sale() {
+
+    }
+}
+
+public class SaleTicketDemo01 {
+
+    /**
+     * 题目: 三个售票员卖出30张票
+     * 笔记: 如何编写企业级的多线程代码——固定的编程套路+模板
+     * 1.在高内聚低耦合的前提下,线程 操作  资源类
+     * 1.1 一言不合,先创建一个资源类
+     */
+    public static void main(String[] args) {
+
+    }
+}
