347f131805fe0ff9192a40fc827787a96a97b1b4
KevenPotter <keven_potter@163.com>
2020-04-10 17:18:17 +0800

    Commit

--- src/main/java/com/example/juc/SaleTicketDemo01.java
+++ src/main/java/com/example/juc/SaleTicketDemo01.java
@@ -1,12 +1,26 @@
 package com.example.juc;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class Ticket { // 资源类 = 实例变量 + 实例方法
 
     private int number = 30;
     // List list = new ArrayList();
 
-    public void sale() {
+    Lock lock = new ReentrantLock(); // 可重入锁
 
+    public void sale() {
+        lock.lock();
+        try {
+            if (number > 0) {
+                System.out.println(Thread.currentThread().getName() + "\t卖出第: " + (number--) + "\t 还剩下: " + number);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
     }
 }
 
